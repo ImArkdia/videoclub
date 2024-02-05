d7e513ebcef5725af92a6d2765a65837016c2379
ImArkdia <jordibmlol@gmail.com>
2024-02-05 20:42:44 +0100

    inicio bbdd

---
+++ database/seeders/MovieSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Movie;
+
+class MovieSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        self::seedCatalog();
+        $this->command->info('Tabla catálogo inicializada con datos!');
+    }
+
+    private function seedCatalog(){
+        require 'array_peliculas.php';
+        foreach($arrayPeliculas as $pelicula){
+            $p = new Movie;
+            $p->title = $pelicula['title'];
+            $p->year = $pelicula['year'];
+            $p->director = $pelicula['director'];
+            $p->poster = $pelicula['poster'];
+            $p->rented = $pelicula['rented'];
+            $p->synopsis = $pelicula['synopsis'];
+            $p->save();
+        }
+    }
+}
