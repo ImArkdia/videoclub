ae21ab998f8736c17309e27613614543ed016168
Jordi <jordibmlol@gmail.com>
2024-02-12 03:41:08 +0100

    end

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,18 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Http\Requests\Auth;
-use Illuminate\Support\Facades\Redirect;
-
 class HomeController extends Controller
 {
     public function getHome(){
-        if(Auth::check()){
             return view('home');
-        }else{
-            return Redirect::to('/login');
-        }
-        
     }
 }
